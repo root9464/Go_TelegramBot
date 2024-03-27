9881200a00a66da22d92df31225dd80abd7e3303
iO <senpai9464@yandex.ru>
2024-03-27 22:57:05 +0300

    27.03.2024 | 22:56 || "Улучшена работа добавлены функции addTask getAllTask"

---
+++ bot/commands/getTask.go
@@ -0,0 +1,26 @@
+package commands
+
+import (
+	"log"
+	"root/database/models"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+func (hb *HomeworkBot) getAllTask(update tgbotapi.Update) {
+	if update.Message.Text == "/getTask" {
+		var tasks []models.Homework
+		err := hb.db.Find(&tasks).Error
+		if err != nil {
+			log.Fatal("ошибка: не удалось получить задания. \n", err)
+		}
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "Ваши задания: \n")
+		for _, task := range tasks {
+			msg.Text += task.SubjectName + " - " + task.Task + "\n"
+
+		}
+		if _, err := hb.bot.Send(msg); err != nil {
+			log.Fatal("ошибка: не удалось отправить сообщение. \n", err)
+		}
+	}
+}
